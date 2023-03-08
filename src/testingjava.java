d2cf1c46f3f001e5f1280fd7fd579efa8d91da4f
ggevo <ggevork1991@gmail.com>
2023-03-07 22:28:19 -0800

    checking INT

---
+++ src/testingjava.java
@@ -0,0 +1,10 @@
+public class testingjava {
+    public static void main(String[] args) {
+
+        int e = 48;
+        int r = 3;
+        int result = e + r;
+
+        System.out.println(result);
+    }
+}
